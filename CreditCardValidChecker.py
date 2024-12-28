d49ca70202da3c61c6098621be1592a335230632
ziad-bakki <bakkizia@msu.edu>
2024-12-28 22:00:19 +0300

    Completed credit card number checker.

---
+++ CreditCardValidChecker.py
@@ -0,0 +1,21 @@
+"""
+Checks if a credit card number is valid using Luhn's algorithm
+"""
+def IsValid(number):
+    number = str(number)
+    number = list(number)
+    for i in range(0, len(number), 2):
+        number[i] = str(int(number[i]) * 2)
+    checksum = ''.join(number)
+    checksum = list(checksum)
+    final = [int(i) for i in checksum]
+    if sum(final) % 10 == 0:
+        return "Valid"
+    else:
+        return "Invalid"
+
+
+
+number = input("Input card number: \n")
+print(f"The card ending in {number[-4:]} is {IsValid(number)}")
+
