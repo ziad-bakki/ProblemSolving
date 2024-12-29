c10d4c724343d5e635215342976000636a01808e
ziad-bakki <bakkizia@msu.edu>
2024-12-29 20:20:48 +0300

    more experimentation with iterating through panda data

--- Weather.py
+++ Weather.py
@@ -4,7 +4,7 @@ from meteostat import Point, Daily
 
 
 start = datetime(2024, 1, 1)
-end = datetime(2024, 2, 1)
+end = datetime(2024, 12, 31)
 
 location = Point(42.7370, -84.4839)
 
@@ -12,8 +12,12 @@ data = Daily(location, start, end)
 data = data.fetch()
 
 count = 0
+minimum_temp = 0
 for index, row in data.iterrows():
-    if row['tmin'] < 0 or row['tmax'] < 0:
-        count += 1
-print(f"It was below 0 degrees celsius {count} times!")
-# print(data)
+    # if row['tmin'] < 0 or row['tmax'] < 0:
+    #     count += 1
+    if row['tmin'] < minimum_temp:
+        minimum_temp = row['tmin']
+# print(f"It was below 0 degrees celsius {count} times!")
+print(f'The lowest temperature in 2023 was {minimum_temp} degrees celsius!')
+
