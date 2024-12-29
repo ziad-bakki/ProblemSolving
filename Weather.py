e26a0903b8a62e41fec51d2484ac3281ca7bf225
ziad-bakki <bakkizia@msu.edu>
2024-12-29 19:25:14 +0300

    experimented with iterating through random temperature data

--- Weather.py
+++ Weather.py
@@ -2,13 +2,18 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from meteostat import Point, Daily
 
-start = datetime(2018, 1, 1)
-end = datetime(2018, 12, 31)
 
-location = Point(49.2497, -123.1193, 70)
+start = datetime(2024, 1, 1)
+end = datetime(2024, 2, 1)
+
+location = Point(42.7370, -84.4839)
 
 data = Daily(location, start, end)
 data = data.fetch()
 
-data.plot(y=['tavg', 'tmin', 'tmax'])
-plt.show()
+count = 0
+for index, row in data.iterrows():
+    if row['tmin'] < 0 or row['tmax'] < 0:
+        count += 1
+print(f"It was below 0 degrees celsius {count} times!")
+# print(data)
