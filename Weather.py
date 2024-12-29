11fc2120dfac69b23645d10eb047abaea751c711
ziad-bakki <bakkizia@msu.edu>
2024-12-29 20:53:32 +0300

    made several functions to get data

--- Weather.py
+++ Weather.py
@@ -3,21 +3,48 @@ import matplotlib.pyplot as plt
 from meteostat import Point, Daily
 
 
-start = datetime(2024, 1, 1)
-end = datetime(2024, 12, 31)
+
 
 location = Point(42.7370, -84.4839)
 
 data = Daily(location, start, end)
 data = data.fetch()
 
-count = 0
-minimum_temp = 0
-for index, row in data.iterrows():
-    # if row['tmin'] < 0 or row['tmax'] < 0:
-    #     count += 1
-    if row['tmin'] < minimum_temp:
-        minimum_temp = row['tmin']
-# print(f"It was below 0 degrees celsius {count} times!")
-print(f'The lowest temperature in 2023 was {minimum_temp} degrees celsius!')
 
+def SetCoordinates(lat, lon):
+    return Point(lat, lon)
+
+def GetMinTemp(data):
+    return min(data.row['tmin'])
+
+def GetMaxTemp(data):
+    return max(data.row['tmax'])
+
+def SubNCount(data : Daily, n : int):
+    count = 0
+    for index, row in data.iterrows():
+        if row['tmin'] < n or row['tmax'] < n:
+            count += 1
+    return count
+
+def SuperNCount(data : Daily, n : int):
+    count = 0
+    for index, row in data.iterrows():
+        if row['tmax'] > n or row['tmin'] > n:
+            count += 1
+    return count
+
+def SetDate(date):
+    return datetime(int(date[0:4]), int(date[5:7]), int(date[8:]))
+
+
+
+def main():
+    start = SetDate(input("Start Date (YYYY/MM/DD):\n"))
+    end = SetDate(input("Start Date (YYYY, MM, DD):\n"))
+    print(start, end)
+    
+
+
+if __name__ == "__main__":
+    main()
